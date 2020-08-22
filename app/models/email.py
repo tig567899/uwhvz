adfff038cfa0f87b7ffd187a84c646269327ca26
UW HvZ <40743370+uwhvz@users.noreply.github.com>
2020-08-21 21:57:38 -0600

    Email formating corrections

--- app/models/email.py
+++ app/models/email.py
@@ -6,15 +6,15 @@ from enumfields import Enum, EnumField
 
 from .game import Game
 
-class EmailRule(Enum):
+class RecipientGroup(Enum):
     HUMAN = 'H'
     ZOMBIE = 'Z'
     ALL = 'A'
 
 
 class EmailManager(models.Manager):
-    def create_email(self, name: str, data : str, rule: EmailRule, game: Game, **extra_fields) -> 'Email':
-        email = self.model(name=name, data=data, rule=rule, game=game, **extra_fields)
+    def create_email(self, name: str, data : str, group: RecipientGroup, game: Game, **extra_fields) -> 'Email':
+        email = self.model(name=name, group=group, rule=rule, game=game, **extra_fields)
         email.save()
         return signup_location
 
@@ -22,9 +22,9 @@ class EmailManager(models.Manager):
 class Email(models.Model):
     id: uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     game: Game = models.ForeignKey(Game, on_delete=models.PROTECT)
-    name: str = models.CharField()
-    data: str = models.CharField()
-    rule: Enum = EnumField(enum=EmailRule, max_length=1)
+    name: str = models.CharField(max_length=256)
+    data: str = models.CharField(max_length=32768)
+    group: Enum = EnumField(enum=RecipientGroup, max_length=1)
     player_made: bool = models.BooleanField(default=False)
 
     created_at: datetime = models.DateTimeField(auto_now_add=True)
